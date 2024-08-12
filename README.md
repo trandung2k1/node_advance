405db306b08823bd9cd846f8e2b830abf63dda97
trandung2k1 <trandungitksnb2001@gmail.com>
2024-08-12 16:35:32 +0700

    update and note thread pool

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Cluster: request error little, expand more requirements
 
 ## Worker thread: multiple threads, handle multiple requests at the same time without blocking between threads
+
+## In essence, the workers created by Cluster use child_process.fork() to communicate with the process that creates them through IPC (Inter-Process Communication).
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const port = 4000;
 import os from 'os';
+import bcrypt from 'bcrypt';
 process.env.UV_THREADPOOL_SIZE = os.cpus().length;
+// process.env.UV_THREADPOOL_SIZE = 1;
 const app = express();
 app.get('/heavy', (req, res) => {
     let total = 0;
@@ -10,8 +12,11 @@ app.get('/heavy', (req, res) => {
     }
     res.send(`The result of the CPU intensive task is ${total}\n`);
 });
-app.get('/', (req, res) => {
-    res.send(`Hi`);
+app.get('/', async (req, res) => {
+    // res.send(`Hi`);
+     const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash('This is my password', salt);
+    return res.send(hashedPassword);
 });
 app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
